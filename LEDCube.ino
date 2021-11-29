58652b355ae4f74fa51557c1add42aa67e6901ed
Dominic West <westdom@mail.com>
2021-11-29 12:14:56 +0000

    Initialising LEDCube with basic Snake pattern

---
+++ LEDCube.ino
@@ -0,0 +1,56 @@
+#include "Snake.h"
+
+unsigned long start_time;
+unsigned long current_time;
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 100;    // the debounce time; increase if the output flickers
+int lastButtonState = LOW;   // the previous reading from the input pin
+int potentiometerPin = A1; // the analog input pin attach to
+int buttonPin = 2;     // the number of the pushbutton pin
+int buttonState = 0;
+int noOfLEDCubePatterns = 1;
+int selectedPattern = 0;
+
+Snake snake("GREEN", "MAGENTA", true, false);
+
+void setup() {
+  pinMode(buttonPin, INPUT);
+  current_time = millis();
+  start_time = current_time;
+}
+
+void loop() {
+  checkButton();
+  current_time = millis();
+  int delay = analogRead(potentiometerPin);
+  if(current_time - start_time >= delay) {
+    start_time = current_time;
+    switch(selectedPattern) {
+      case 0:
+        snake.update();
+        break;
+    }
+  }
+}
+
+void checkButton() {
+  int reading = digitalRead(buttonPin);
+  if(reading != lastButtonState) {
+    lastDebounceTime = millis();
+  }
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (reading != buttonState) {
+      buttonState = reading;
+      if(buttonState == HIGH) {
+        cycleToNextPattern();
+      }
+    }
+  }
+}
+
+void cycleToNextPattern() {
+  selectedPattern++;
+  if(selectedPattern >= noOfLEDCubePatterns) {
+    selectedPattern = 0;
+  }
+}
---
+++ Snake.h
@@ -0,0 +1,39 @@
+#ifndef Snake_H
+#define Snake_H
+
+#include <Arduino.h>
+
+class Snake {
+
+  String snakeColour;
+  String pelletColour;
+  bool pelletOn;
+  bool travelThroughWallsOn;
+  
+  public:
+    Snake(String snakeColour, String pelletColour, bool pelletOn, bool travelThroughWallsOn);
+    void update();
+    
+  private:
+    void setup();
+    void setPositionsMatrix();
+    void moveSnakeTail();
+    void moveSnakeHeadAndPellet();
+    void setPossibleMoves(bool moves[], String legalMoves[], int legalMovesSize);
+    bool canMove(String move);
+    bool moveHeadToPill(bool validMoves[], String legalMoves[], int legalMovesSize);
+    void moveHeadInRandomValidDir(bool validMoves[], String legalMoves[]);
+    void movePelletToRandomUnpopulatedLED();
+    int getAxisIndex(String move);
+    char getAxis(String move);
+    int getAxisDirection(String move);
+    bool isIncrementMove(String move);
+    void setCubeToPositionsColours(int positions[][3], int noOfPositions, int positionsColours[][3], String offColour);
+    bool arrayEquals(const int arr1[], int arr1Size, const int arr2[], int arr2Size);
+    bool matrixContains(int matrix[][3], int matrixSize, int arr[3]);
+    int findIndexOfString(String string, const String arr[], int arrSize);
+    void sendColour(byte r, byte g, byte b);
+    void sendByte(byte b);
+};
+
+#endif
